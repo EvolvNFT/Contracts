1eba6dcbfb3fa3a6f12e7cfb3696f2d7897ef37f
Bibek Koirala <bibekkoirala54@gmail.com>
2022-05-10 23:42:05 +0545

    Accepts any token for sales

--- contracts/Factory.sol
+++ contracts/Factory.sol
@@ -11,6 +11,19 @@ contract Factory {
     struct Brand {
         string name;
         address owner;
+        address treasury;//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+import "./XenonNFT.sol";
+import "hardhat/console.sol";
+
+contract Factory {
+
+    address private owner;
+    address private oracle;
+    struct Brand {
+        string name;
+        address owner;
         address treasury;
         address nftContract;
         bool isActive;
@@ -37,20 +50,122 @@ contract Factory {
         oracle = _oracle;
     }
 
-    function onboardBrand(string memory _brandId, string memory _brandName, address _owner, address _treasury, uint256 _salePrice, uint256 _salesStartBlock, uint256 _salesEndBlock) public onlyOwner{
-        require(!brands[_brandId].isActive, "Brand already exists");
-        console.log("Adding a brand with name '%s' owner '%s' and treasury '%s'", _brandName, _owner, _treasury);
-        address nftAddress = address(new LevelNFT(_brandId, _brandName, _salePrice, _treasury, uint256 _salesStartBlock, uint256 _salesEndBlock));
-        console.log(nftAddress);
+    function onboardBrand(
+        string memory _brandId,
+        string memory _brandName,
+        address _owner,
+        address _treasury,
+        uint256 _salePrice,
+        uint256 _salesStartBlock,
+        uint256 _salesEndBlock,
+        bool _isTokenSale,
+        address _salesTokenAddress
+        ) public onlyOwner{
+            require(!brands[_brandId].isActive, "Brand already exists");
+            
+            console.log("Adding a brand with name '%s' owner '%s' and treasury '%s'", _brandName, _owner, _treasury);
+            address nftAddress = address(new LevelNFT(_brandId, _brandName, _salePrice, _treasury, _salesStartBlock, _salesEndBlock, _isTokenSale, _salesTokenAddress));
+            console.log(nftAddress);
 
-        brands[_brandId] = Brand(_brandName, _owner, _treasury, nftAddress, true);
+            brands[_brandId] = Brand(_brandName, _owner, _treasury, nftAddress, true);
     }
 
-    function onboardExistingBrand(string memory _brandId, string memory _brandName, address _owner, address _treasury, address nftAddress) public onlyOwner{
-        require(!brands[_brandId].isActive, "Brand already exists");
-        console.log("Adding a brand with name '%s' owner '%s' and treasury '%s'", _brandName, _owner, _treasury);
+    function onboardExistingBrand(
+        string memory _brandId,
+        string memory _brandName,
+        address _owner,
+        address _treasury,
+        address nftAddress
+        ) public onlyOwner{
+            require(!brands[_brandId].isActive, "Brand already exists");
+            console.log("Adding a brand with name '%s' owner '%s' and treasury '%s'", _brandName, _owner, _treasury);
 
-        brands[_brandId] = Brand(_brandName, _owner, _treasury, nftAddress, true);
+            brands[_brandId] = Brand(_brandName, _owner, _treasury, nftAddress, true);
+    }
+
+    function levelUpNFT(address nftAddress, uint256 tokenId) public onlyOracle{
+        nftLevels[nftAddress][tokenId] += 1;
+    }
+
+    function levelUpNFTByUser(address nftAddress, uint256 tokenId) public payable{
+        nftLevels[nftAddress][tokenId] += 1;
+    }
+
+    function getOracle() public view returns (address) {
+        return oracle;
+    }
+
+    function getAdmin() public view returns (address) {
+        return owner;
+    }
+
+    function setOracle(address _oracle) public onlyOwner{
+        console.log("Changing oracle from '%s' to '%s'", oracle, _oracle);
+        oracle = _oracle;
+    }
+
+    function changeAdmin(address _admin) public onlyOwner{
+        console.log("Changing admin from '%s' to '%s'", owner, _admin);
+        owner = _admin;
+    }
+}
+
+        address nftContract;
+        bool isActive;
+    }
+
+    mapping (string => Brand) public brands;
+
+    mapping (address => mapping(uint256 => uint256)) nftLevels;
+    mapping (address => uint256) addressLevels;
+
+    modifier onlyOracle {
+        require(msg.sender == oracle);
+        _;
+    }
+
+    modifier onlyOwner {
+        require(msg.sender == owner);
+        _;
+    }
+
+    constructor(address _owner, address _oracle) {
+        console.log("Deploying a Factory with owner:", _owner);
+        owner = _owner;
+        oracle = _oracle;
+    }
+
+    function onboardBrand(
+        string memory _brandId,
+        string memory _brandName,
+        address _owner,
+        address _treasury,
+        uint256 _salePrice,
+        uint256 _salesStartBlock,
+        uint256 _salesEndBlock,
+        bool _isTokenSale,
+        address _salesTokenAddress
+        ) public onlyOwner{
+            require(!brands[_brandId].isActive, "Brand already exists");
+            
+            console.log("Adding a brand with name '%s' owner '%s' and treasury '%s'", _brandName, _owner, _treasury);
+            address nftAddress = address(new LevelNFT(_brandId, _brandName, _salePrice, _treasury, _salesStartBlock, _salesEndBlock, _isTokenSale, _salesTokenAddress));
+            console.log(nftAddress);
+
+            brands[_brandId] = Brand(_brandName, _owner, _treasury, nftAddress, true);
+    }
+
+    function onboardExistingBrand(
+        string memory _brandId,
+        string memory _brandName,
+        address _owner,
+        address _treasury,
+        address nftAddress
+        ) public onlyOwner{
+            require(!brands[_brandId].isActive, "Brand already exists");
+            console.log("Adding a brand with name '%s' owner '%s' and treasury '%s'", _brandName, _owner, _treasury);
+
+            brands[_brandId] = Brand(_brandName, _owner, _treasury, nftAddress, true);
     }
 
     function levelUpNFT(address nftAddress, uint256 tokenId) public onlyOracle{
--- contracts/XenonNFT.sol
+++ contracts/XenonNFT.sol
@@ -1,11 +1,13 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Royalty.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "hardhat/console.sol";
 
-contract LevelNFT is ERC721Royalty{
+contract LevelNFT is ERC721Royalty {
 
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
@@ -14,13 +16,17 @@ contract LevelNFT is ERC721Royalty{
     uint256 private salePrice;
     uint256 private salesStartBlock;
     uint256 private salesEndBlock;
+    bool private isTokenSale;
+    address private salesTokenAddress;
+
+    IERC20 salesToken;
 
     modifier onlyTreasury {
         require(msg.sender == treasury);
         _;
     }
 
-    modifier validateSalesTime{
+    modifier validateSalesTime {
         require(salesStartBlock <= block.number && salesEndBlock >= block.number);
         _;
     }
@@ -30,15 +36,33 @@ contract LevelNFT is ERC721Royalty{
         _;
     }
 
-    constructor(string memory _name, string memory _symbol, uint256 _salePrice, address _treasury, uint256 _salesStartBlock, uint256 _salesEndBlock) ERC721(_name, _symbol){
-        salePrice = _salePrice;
-        treasury = _treasury;
-        salesStartBlock = _salesStartBlock;
-        salesEndBlock = _salesEndBlock;
+    constructor(
+        string memory _name,
+        string memory _symbol,
+        uint256 _salePrice,
+        address _treasury,
+        uint256 _salesStartBlock,
+        uint256 _salesEndBlock,
+        bool _isTokenSale,
+        address _salesTokenAddress
+        ) ERC721(_name, _symbol){
+
+            require(block.number <= _salesStartBlock && _salesStartBlock < _salesEndBlock, "Sale Timings not applicable    ");
+
+            salePrice = _salePrice;
+            treasury = _treasury;
+            salesStartBlock = _salesStartBlock;
+            salesEndBlock = _salesEndBlock;
+            isTokenSale = _isTokenSale;
+            salesTokenAddress = _salesTokenAddress;
+
+            salesToken = IERC20(_salesTokenAddress);
     }
 
-    function buyNFT() public payable returns (uint256) validateSalesTime{
+    function buyNFTWithEth() public payable validateSalesTime returns (uint256) {
+        require( isTokenSale == false, "Sale is token-based");
         require(msg.value >= salePrice);
+
         uint256 newNFTId = _tokenIds.current();
         super._safeMint(msg.sender, newNFTId);
 
@@ -46,11 +70,34 @@ contract LevelNFT is ERC721Royalty{
         return newNFTId;
     }
 
-    function claimSalesAmount() public onlyTreasury validateAfterSalesTime{
+    function buyNFTWithToken() public validateSalesTime returns (uint256) {
+        require( isTokenSale == true, "Sale is eth-based");
+
+        uint256 newNFTId = _tokenIds.current();
+        super._safeMint(msg.sender, newNFTId);
+
+        _tokenIds.increment();
+
+        salesToken.transferFrom(msg.sender, address(this), salePrice);
+        return newNFTId;
+    }
+
+    function claimSalesEthAmount() public validateAfterSalesTime onlyTreasury {
+        require( isTokenSale == false, "Sale is token-based");
+
         uint256 contractBalance = address(this).balance;
         address payable treasuryAddress = payable(treasury);
         if( contractBalance > 0 ) {
             treasuryAddress.transfer(contractBalance);
+        }
+    }
+
+    function claimSalesTokenAmount() public onlyTreasury validateAfterSalesTime {
+        require( isTokenSale == true, "Sale is eth-based");
+
+        uint256 contractBalance = salesToken.balanceOf(address(this));
+        if( contractBalance > 0 ) {
+            salesToken.transfer(msg.sender, contractBalance);
         }
     }
 }
