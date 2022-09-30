673f51fe7d740b7432f46b7a8ca208bed16c4c2f
Karan <19ucc025@lnmiit.ac.in>
2022-09-30 19:23:03 +0530

    Marketplace Updated and Tested

--- contracts/Marketplace.sol
+++ contracts/Marketplace.sol
@@ -4,7 +4,6 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 
-
 import "./interfaces/IXenonNFT.sol";
 
 import "./XenonNFT.sol";
@@ -13,15 +12,19 @@ import "hardhat/console.sol";
 
 contract MarketPlace {
 
-    mapping(address=>uint256) balancesOfNFT;
+    address payable treasury;
+
+    constructor(address payable _treasury){
+        treasury=_treasury;
+    }
+
     mapping(address=>uint256) userBalance;
     
     struct NFT{
-        uint256 minPrice;
+        uint256 salePrice;
         uint256 endTime;
-        uint256 bid;
-        address payable seller;
-        address payable bidder; 
+        address payable owner;
+        address payable buyer;
         bool onSale;
         bool isEthSale;
         address salesTokenAddress;        
@@ -38,134 +41,106 @@ contract MarketPlace {
         locked=false;
     }
 
-    function listOnAuction(address nftAddress, uint256 tokenID, uint256 _endTime, uint256 salePrice,bool _isETHSale, address _salesTokenAddress) public{
+    function listNFTforSale(address nftAddress, uint256 tokenID, uint256 _endTime, uint256 salePrice, bool _isETHSale, address _salesTokenAddress) public{
         
         IXenonNFT nft = IXenonNFT(nftAddress);
 
-        require(msg.sender==nft.ownerOf(tokenID),"hatt yaar");
-        require(NFTs[nftAddress][tokenID].onSale==false,'Already on Sale');
+        require(msg.sender==nft.ownerOf(tokenID),"Error: You must be the owner of NFT");
+        require(NFTs[nftAddress][tokenID].onSale==false,'Error: The NFT is already on Sale');
 
         nft.transferFrom(msg.sender, address(this), tokenID);
 
-        NFTs[nftAddress][tokenID].minPrice = salePrice;
+        NFTs[nftAddress][tokenID].salePrice = salePrice;
         NFTs[nftAddress][tokenID].endTime = _endTime;
-        NFTs[nftAddress][tokenID].bid = 0;
-        NFTs[nftAddress][tokenID].seller = payable(msg.sender);
+        NFTs[nftAddress][tokenID].owner = payable(msg.sender);
         NFTs[nftAddress][tokenID].onSale = true;
         NFTs[nftAddress][tokenID].isEthSale = _isETHSale;
         NFTs[nftAddress][tokenID].salesTokenAddress = _salesTokenAddress;
     }
 
-    function withdrawFromAuction(address nftAddress, uint256 tokenID) public{
+    function revertNFTfromSale(address nftAddress, uint256 tokenID) public{
         
         IXenonNFT nft = IXenonNFT(nftAddress);
 
-        require(msg.sender==NFTs[nftAddress][tokenID].seller);
-        require(NFTs[nftAddress][tokenID].onSale==true,'NFT is not on sale');
+        require(msg.sender==NFTs[nftAddress][tokenID].owner,'Error: You must be the owner of NFT');
+        require(NFTs[nftAddress][tokenID].onSale==true,'Error: NFT must be on sale to cancel listing');
+        
         nft.transferFrom(address(this),msg.sender,tokenID);
 
         NFTs[nftAddress][tokenID].onSale=false;
     }
 
 
-    function bidWithETH(address nftAddress, uint256 tokenID) public payable{
-        IERC721 nft = IERC721(nftAddress);
+    function buyWithETH(address nftAddress, uint256 tokenID) public payable{
+        IXenonNFT nft = IXenonNFT(nftAddress);
 
-        require(nft.ownerOf(tokenID)!=msg.sender,'owner cannot bid');
-        require(NFTs[nftAddress][tokenID].isEthSale==true,'Not ETH sale');
-        require(NFTs[nftAddress][tokenID].onSale==true,'Not on Sale');
-        require(NFTs[nftAddress][tokenID].endTime>block.timestamp, 'Sale ended');
+        require(NFTs[nftAddress][tokenID].owner!=msg.sender,'Error: The owner cannot buy the NFT');
+        require(NFTs[nftAddress][tokenID].isEthSale==true,'Error: Please switch to token Sale');
+        require(NFTs[nftAddress][tokenID].onSale==true,'Error: NFT is not listed on Sale');
+        require(NFTs[nftAddress][tokenID].endTime>block.timestamp, 'Error: The Sale for NFT has ended');
 
-            if(NFTs[nftAddress][tokenID].bid==0){
-                require(msg.value>=NFTs[nftAddress][tokenID].minPrice,'value sent is lower than minimum first bid');
-            }
-            else{
-                require(msg.value>NFTs[nftAddress][tokenID].bid,'value sent is lower than current bid');
-            }
-            userBalance[NFTs[nftAddress][tokenID].seller]+=msg.value;
-            NFTs[nftAddress][tokenID].bidder=payable(msg.sender);
-            NFTs[nftAddress][tokenID].bid=msg.value;
-    }
+        require(msg.value>=NFTs[nftAddress][tokenID].salePrice,"Error: Does not meet the Sale Price Cost");
 
-    function bidWithToken(address nftAddress, uint256 tokenID) public payable{
-        IERC721 nft = IERC721(nftAddress);
+        NFTs[nftAddress][tokenID].buyer = payable(msg.sender);
 
-        require(nft.ownerOf(tokenID)!=msg.sender,'owner cannot bid');
-        require(NFTs[nftAddress][tokenID].isEthSale==false,'not a token based sale');
-        require(NFTs[nftAddress][tokenID].onSale==true,'Not on Sale');
-        require(NFTs[nftAddress][tokenID].endTime>block.timestamp, 'Sale ended');
-
-            IERC20 salesToken = IERC20(NFTs[nftAddress][tokenID].salesTokenAddress);
-            
-            if(NFTs[nftAddress][tokenID].bid==0){
-                require(salesToken.balanceOf(msg.sender)>=NFTs[nftAddress][tokenID].minPrice,'value sent is lower than minimum first bid');
-            }
-            else{
-                require(salesToken.balanceOf(msg.sender)>NFTs[nftAddress][tokenID].bid,'value sent is lower than current bid');
-            }
-            userBalance[NFTs[nftAddress][tokenID].seller]+=salesToken.balanceOf(address(this));
-            NFTs[nftAddress][tokenID].bidder=payable(msg.sender);
-            NFTs[nftAddress][tokenID].bid=salesToken.balanceOf(msg.sender);
-
-            salesToken.transferFrom(msg.sender, address(this), NFTs[nftAddress][tokenID].bid);
-
-    }
-
-    function claim(address nftAddress, uint256 tokenID) public noReentrant{
-
-        IERC721 nft = IERC721(nftAddress);
-
-        require(msg.sender==NFTs[nftAddress][tokenID].bidder,'You are not the hightest bidder');
-        require(NFTs[nftAddress][tokenID].onSale==true,'Not on Sale');
-        require(NFTs[nftAddress][tokenID].endTime<block.timestamp, 'Sale has not ended');
+        userBalance[NFTs[nftAddress][tokenID].owner]+=((msg.value)*(95))/100;
+        userBalance[treasury]+=((msg.value)*(5))/100;
 
         NFTs[nftAddress][tokenID].onSale=false;
 
-        nft.transferFrom(address(this),NFTs[nftAddress][tokenID].bidder,tokenID);
-
-        _transfer(NFTs[nftAddress][tokenID].seller, NFTs[nftAddress][tokenID].bidder, nftAddress, tokenID);
-
-        userBalance[NFTs[nftAddress][tokenID].seller]+=NFTs[nftAddress][tokenID].bid;
-        userBalance[NFTs[nftAddress][tokenID].bidder]-=NFTs[nftAddress][tokenID].bid;
+        nft.transferFrom(address(this),NFTs[nftAddress][tokenID].buyer,tokenID);
 
     }
 
-    function _transfer(address from, address to, address nftAddress, uint256 tokenID) internal {
-        require(to!= address(0));
-        require(NFTs[nftAddress][tokenID].onSale == false);  // not transfer NFT if it is listed on Auction
+    function buyWithToken(address nftAddress, uint256 tokenID) public{
+        IXenonNFT nft = IXenonNFT(nftAddress);
 
-        balancesOfNFT[from]--;
-        balancesOfNFT[to]++;
+        require(NFTs[nftAddress][tokenID].owner!=msg.sender,'Error: The owner cannot buy the NFT');
+        require(NFTs[nftAddress][tokenID].isEthSale==true,'Error: Please switch to token Sale');
+        require(NFTs[nftAddress][tokenID].onSale==true,'Error: NFT is not listed on Sale');
+        require(NFTs[nftAddress][tokenID].endTime>block.timestamp, 'Error: The Sale for NFT has ended');
+
+        IERC20 salesToken = IERC20(NFTs[nftAddress][tokenID].salesTokenAddress);
+            
+        require(salesToken.balanceOf(msg.sender)>=NFTs[nftAddress][tokenID].salePrice,'Error: value sent is lower than minimum cost');
+            
+        userBalance[NFTs[nftAddress][tokenID].owner]+=(salesToken.balanceOf(address(this))*95)/100;
+        userBalance[treasury]+=(salesToken.balanceOf(address(this))*5)/100;
+        userBalance[NFTs[nftAddress][tokenID].buyer]+=salesToken.balanceOf(address(this));
+
+        NFTs[nftAddress][tokenID].buyer=payable(msg.sender);
+
+        salesToken.transferFrom(msg.sender, address(this), NFTs[nftAddress][tokenID].salePrice);
+
+        NFTs[nftAddress][tokenID].onSale=false;
+
+        nft.transferFrom(address(this),NFTs[nftAddress][tokenID].buyer,tokenID);
 
     }
 
-    function withDrawETH() public{
+    function withdrawYourETH() public{
         uint256 balance = userBalance[msg.sender];
-        require(balance>0,'There is no ETH to transfer');
+        require(balance>0,'Error: There is no ETH to transfer');
 
         address payable _treasuryAddress = payable(msg.sender);
 
         (bool _sent,) = _treasuryAddress.call{value: balance}("");
-        require(_sent, "Failed to send Ether");
+        require(_sent, "Error: Failed to send Ether");
 
         userBalance[msg.sender]=0;
     }
 
-    function withdrawToken(address _salesToken) public{
+    function withdrawYourToken(address _salesToken) public{
         IERC20 salesToken = IERC20(_salesToken);
 
         uint256 _contractBalance = salesToken.balanceOf(msg.sender);
-        require( _contractBalance > 0 , "No Token accumulated");
+        require( _contractBalance > 0 , "Error: No Token accumulated");
         salesToken.transfer(msg.sender, _contractBalance);
 
     }
 
     function getBalance() public view returns(uint256){
         return userBalance[msg.sender];
-    }
-
-    function getNftBidStatus(address nftAddress, uint256 tokenID) public view returns(address,uint256){
-        return (NFTs[nftAddress][tokenID].bidder,NFTs[nftAddress][tokenID].bid);
     }
 
 }
